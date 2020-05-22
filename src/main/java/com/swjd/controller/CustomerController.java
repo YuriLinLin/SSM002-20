bf30f1a5e7260e78766858c33510580bf2042644
YuriLinLin <610195478@qq.com>
2020-05-22 11:04:49 +0800

    添加打印语句

--- src/main/java/com/swjd/controller/CustomerController.java
+++ src/main/java/com/swjd/controller/CustomerController.java
@@ -18,6 +18,7 @@ public class CustomerController {
 
     @RequestMapping("/toMain")
     public  String toMain(Model model){
+        System.out.println("123");
         List<Customer> list=new ArrayList<>();
         list=customerService.getList();
         Customer customer=new Customer();
